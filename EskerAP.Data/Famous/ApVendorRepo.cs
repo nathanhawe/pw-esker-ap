a9ac8922cb134639d31314de0965b3540ab526b3
Nathan Hawe <nathanhawe@gmail.com>
2022-09-28 13:19:00 -0700

    Create Famous AP Vendor repo and tests.

---
+++ EskerAP.Data/Famous/ApVendorRepo.cs
@@ -0,0 +1,142 @@
+﻿using EskerAP.Domain;
+using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+
+namespace EskerAP.Data.Famous
+{
+	public class ApVendorRepo : IApVendorRepo
+	{
+		private readonly string _connectionString;
+		private readonly string _schema;
+		private readonly bool _hasSchema;
+		private readonly ILogger<ApVendorRepo> _logger;
+
+		public ApVendorRepo(string connectionString, string schema, ILogger<ApVendorRepo> logger)
+		{
+			_connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+			_schema = schema?.Trim();
+			_hasSchema = !String.IsNullOrWhiteSpace(schema);
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public IEnumerable<Domain.Vendor> GetVendors()
+		{
+			_logger.LogDebug("GetVendors() Invoked");
+
+			var accounts = new List<Vendor>();
+			using OracleConnection con = new OracleConnection(_connectionString);
+			using OracleCommand cmd = con.CreateCommand();
+			try
+			{
+				con.Open();
+				cmd.BindByName = true;
+				cmd.CommandText = GetQuery();
+				OracleDataReader reader = cmd.ExecuteReader();
+
+				Vendor record;
+				while (reader.Read())
+				{
+					record = new Vendor
+					{
+						Id = reader.IsDBNull(0) ? "" : reader.GetString(0),
+						Name = reader.IsDBNull(1) ? "" : reader.GetString(1),
+						PhoneNumber = reader.IsDBNull(2) ? "" : reader.GetString(2),
+						FaxNumber = reader.IsDBNull(3) ? "" : reader.GetString(3),
+						DUNSNumber = reader.IsDBNull(4) ? "" : reader.GetString(4),
+						PaymentTermsCode = reader.IsDBNull(5) ? "" : reader.GetString(5),
+						Email = reader.IsDBNull(6) ? "" : reader.GetString(6),
+						Street = reader.IsDBNull(7) ? "" : reader.GetString(7),
+						City = reader.IsDBNull(8) ? "" : reader.GetString(8),
+						PostalCode = reader.IsDBNull(9) ? "" : reader.GetString(9),
+						Region = reader.IsDBNull(10) ? "" : reader.GetString(10),
+						Country = reader.IsDBNull(11) ? "" : reader.GetString(11),
+					};
+					accounts.Add(record);
+				}
+				reader.Dispose();
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError("An exception occured while attempting to get Vendors from Famous: {Message}.", ex.Message);
+			}
+
+			return accounts;
+
+		}
+
+		private string GetQuery()
+		{
+			return $@"SELECT
+				  Name.ID AS VendorId
+				, Name.LASTCONAME AS Name
+				,( SELECT Voice FROM 
+					(SELECT
+						TO_NCHAR(AREACODE) || TRIM(BOTH FROM COMMSTRING) AS Voice 
+					FROM {(_hasSchema ? _schema + "." : "")}FC_NAME_COMM fnc
+					WHERE 
+						fnc.NAMEIDX = Vendor.VENDNAMEIDX 
+						AND fnc.INACTIVEFLAG = 'N'
+						AND fnc.COMMTYPE = 2 -- 2 = Voice, 3 = Fax (no area code field), 4 = Email
+					ORDER BY 
+						  fnc.NAMECOMMSEQ ASC
+						, fnc.ORDERBY) 
+					WHERE ROWNUM = 1
+				) AS PhoneNumber
+				,( SELECT COMMSTRING FROM 
+					(SELECT
+						COMMSTRING 
+					FROM {(_hasSchema ? _schema + "." : "")}FC_NAME_COMM fnc
+					WHERE 
+						fnc.NAMEIDX = Vendor.VENDNAMEIDX 
+						AND fnc.INACTIVEFLAG = 'N'
+						AND fnc.COMMTYPE = 3 -- 2 = Voice, 3 = Fax (no area code field), 4 = Email
+					ORDER BY 
+						  fnc.NAMECOMMSEQ ASC
+						, fnc.ORDERBY) 
+					WHERE ROWNUM = 1
+				) AS FaxNumber
+				-- VATNumber
+				, NameLocation.DUNSNO AS DUNSNumber
+				-- PreferredInvoiceType
+				, PayTerms.DESCR AS PaymentTermCode
+				,( SELECT COMMSTRING FROM 
+					(SELECT
+						COMMSTRING 
+					FROM {(_hasSchema ? _schema + "." : "")}FC_NAME_COMM fnc
+					WHERE 
+						fnc.NAMEIDX = Vendor.VENDNAMEIDX 
+						AND fnc.INACTIVEFLAG = 'N'
+						AND fnc.COMMTYPE = 4 -- 2 = Voice, 3 = Fax (no area code field), 4 = Email
+					ORDER BY 
+						  fnc.NAMECOMMSEQ ASC
+						, fnc.ORDERBY) 
+					WHERE ROWNUM = 1
+				) AS Email
+				-- General Account
+				-- TaxSystem
+				-- Currency
+				-- Parafiscal Tax
+				-- Supplier Due
+				-- Sub
+				, NameLocation.ADDRESS AS Street
+				-- Post Office Box
+				, NameLocation.CITY 
+				, NameLocation.ZIP  AS PostalCode
+				, NameLocation.STATE  AS Region
+				, NameLocation.COUNTRY
+			FROM {(_hasSchema ? _schema + "." : "")}AP_VENDOR Vendor
+				LEFT JOIN {(_hasSchema ? _schema + "." : "")}FC_NAME Name ON 
+					Vendor.VENDNAMEIDX = Name.NAMEIDX
+				LEFT JOIN {(_hasSchema ? _schema + "." : "")}FC_NAME_LOCATION NameLocation ON
+					Name.NAMEIDX = NameLocation.NAMEIDX 
+					AND NameLocation.INACTIVEFLAG = 'N'
+					AND NameLocation.NAMELOCATIONSEQ = 1
+				LEFT JOIN {(_hasSchema ? _schema + "." : "")}AP_PAY_TERMS PayTerms ON
+					Vendor.APPAYTERMSIDX = PayTerms.APPAYTERMSIDX 
+			ORDER BY 
+				Name.ID";
+		}
+	}
+}
---
+++ EskerAP.Domain/Vendor.cs
@@ -0,0 +1,115 @@
+﻿namespace EskerAP.Domain
+{
+	public class Vendor
+	{
+		/// <summary>
+		/// The unique identifier for the specific company defined in Esker
+		/// </summary>
+		public string CompanyCode { get; set; }
+
+		/// <summary>
+		/// The unique identifier of the vendor
+		/// </summary>
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Name of the vendor
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Internationalized phone number (e.g., +33123457896)
+		/// </summary>
+		public string PhoneNumber { get; set; }
+
+		/// <summary>
+		/// Internationalized fax Number (e.g., +33123457896)
+		/// </summary>
+		public string FaxNumber { get; set; }
+
+		/// <summary>
+		/// Internationalized VAT number
+		/// </summary>
+		public string VATNumber { get; set; }
+
+		/// <summary>
+		/// Unique nine-digit identifier of the vendor's company
+		/// </summary>
+		public string DUNSNumber { get; set; }
+
+		/// <summary>
+		///  Default invoice type emitted by this vendor.
+		/// </summary>
+		public string PreferredInvoiceType { get; set; }
+
+		/// <summary>
+		/// Code for the vendor's payment terms
+		/// </summary>
+		public string PaymentTermsCode { get; set;}
+
+		/// <summary>
+		/// Vendor's email address
+		/// </summary>
+		public string Email { get; set; }
+
+		/// <summary>
+		/// Number of the collective G/L account associated with one or several vendors
+		/// </summary>
+		public string GeneralAccount { get; set; }
+
+		/// <summary>
+		/// code of the tax system applicable to the vendor
+		/// </summary>
+		public string TaxSystem { get; set; }
+
+		/// <summary>
+		/// Code for the vendor's currency.
+		/// </summary>
+		public string Currency { get; set; }
+
+		/// <summary>
+		/// Specifies whether the parafiscal tax is application to the vendor or not
+		/// </summary>
+		public bool ParafiscalTax { get; set; } = false;
+
+		/// <summary>
+		/// Code for the vendor's VAT type.
+		/// </summary>
+		public string SupplierDue { get; set; }
+
+		/// <summary>
+		/// Address additional details
+		/// </summary>
+		public string Sub { get; set; }
+
+		/// <summary>
+		/// Vendor's street address
+		/// </summary>
+		public string Street { get; set; }
+
+		/// <summary>
+		/// Vendor's locality or post office box
+		/// </summary>
+		public string PostOfficeBox { get; set; }
+
+		/// <summary>
+		/// Vendor's city
+		/// </summary>
+		public string City { get; set; }
+
+		/// <summary>
+		/// Vendor's ZIP code
+		/// </summary>
+		public string PostalCode { get; set; }
+
+		/// <summary>
+		/// State or region code of the vendor
+		/// </summary>
+		public string Region { get; set; }
+
+		/// <summary>
+		/// Vendor's country (ISO 3166-1 code)
+		/// </summary>
+		public string Country { get; set; }
+	}
+}
