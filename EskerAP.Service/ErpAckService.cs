53c5a9d381d4af5eed16c24b266fe87eb1ac0fd1
Nathan Hawe <nathanhawe@gmail.com>
2023-01-06 13:47:28 -0800

    Create service that generates the ERP acknowledgement  XML string.

---
+++ EskerAP.Service/ErpAckService.cs
@@ -0,0 +1,52 @@
+﻿using EskerAP.Domain;
+using EskerAP.Service.Interface;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace EskerAP.Service
+{
+	public class ErpAckService : IErpAckService
+	{
+		private readonly ILogger<ErpAckService> _logger;
+
+		public ErpAckService(ILogger<ErpAckService> logger)
+		{
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public string GetErpAckXmlString(ImportApVoucherResponse importApVoucherResponse, string ruid)
+		{
+			_logger.LogDebug("Attempting to create an ERP Ack XML string for {importApVoucherResponse}", importApVoucherResponse);
+			if (importApVoucherResponse == null) throw new ArgumentNullException(nameof(importApVoucherResponse));
+
+			return $@"<?xml version=""1.0"" encoding=""utf-8""?><ERPAck><EskerInvoiceID>{ruid}</EskerInvoiceID>{(importApVoucherResponse.ImportWasSuccessful ? GetERPIDElement(importApVoucherResponse) : GetERPPostingErrorElement(importApVoucherResponse))}</ERPAck>";
+		}
+
+		private string GetERPPostingErrorElement(ImportApVoucherResponse importApVoucherResponse)
+		{
+			var message = "";
+
+			// Exceptions
+			if (importApVoucherResponse.Exception != null) message += importApVoucherResponse.Exception.Message;
+
+			// Header
+			if (importApVoucherResponse.HeaderErrors != null) message += $"Header: {importApVoucherResponse.HeaderErrors}";
+
+			// Other
+			if (importApVoucherResponse.OtherErrors != null) message += $"Other: {importApVoucherResponse.OtherErrors}";
+
+			// Line
+			if (importApVoucherResponse.LineErrors.Count > 0) message += "Lines: ";
+			foreach (var lineError in importApVoucherResponse.LineErrors) message += $"{lineError};";
+
+			if (message.Length > 1024) message = message[..1024];
+
+			return $@"<ERPPostingError>{message}</ERPPostingError>";
+		}
+
+		private string GetERPIDElement(ImportApVoucherResponse importApVoucherResponse)
+		{
+			return $"<ERPID>{importApVoucherResponse.EntryNumber}</ERPID>";
+		}
+	}
+}
---
+++ EskerAP.Service/Interface/IErpAckService.cs
@@ -0,0 +1,12 @@
+﻿using EskerAP.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EskerAP.Service.Interface
+{
+	public interface IErpAckService
+	{
+		string GetErpAckXmlString(ImportApVoucherResponse importApVoucherResponse, string ruid);
+	}
+}
