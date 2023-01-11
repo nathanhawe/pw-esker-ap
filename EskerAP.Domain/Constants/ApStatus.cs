708552c2f2dbef139e53178ae93efb188b9520cf
Nathan Hawe <nathanhawe@gmail.com>
2023-01-10 16:46:18 -0800

    Create repository to pull paid invoices from Famous.

---
+++ EskerAP.Domain/Constants/ApStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace EskerAP.Domain.Constants
+{
+	/// <summary>
+	/// Famous AP Status field values
+	/// </summary>
+	public static class ApStatus
+	{
+		public static string Entered = "1";
+		public static string Unknown = "2";
+		public static string RunSelected = "3";
+		public static string Completed = "4";
+	}
+}
---
+++ EskerAP.Domain/Constants/PaymentMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace EskerAP.Domain.Constants
+{
+	/// <summary>
+	/// Esker Payment Method values used in updating invoice payment status.
+	/// </summary>
+	public static class PaymentMethod
+	{
+		public static string Cash = "Cash";
+		public static string Check = "Check";
+		public static string CreditCard = "Credit card";
+		public static string Eft = "EFT";
+		public static string Other = "Other";
+	}
+}
